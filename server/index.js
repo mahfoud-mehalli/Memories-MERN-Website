1d7d6c1bd7dff25d530e07ceaf6033e062e24aea
mahfoud-mehalli <mahfoudmehalli12345@gmail.com>
2024-03-11 19:58:29 +0100

    connecting to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,4 +1,22 @@
 import express from 'express'
-import bodyparser from 'body-parser'
+import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://mahfoudmehalli12345:mahfoudmehalli12345@memories-mern-website.dhaqibp.mongodb.net/?retryWrites=true&w=majority&appName=Memories-MERN-Website'
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+
+
+
+// https://www.mongodb.com/cloud/atlas
