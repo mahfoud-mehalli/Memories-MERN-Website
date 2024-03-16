2881e150d4ff046de01447beabe0b3be095a716a
mahfoud-mehalli <mahfoudmehalli12345@gmail.com>
2024-03-16 22:59:57 +0100

    post and posts jsx

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,6 @@
 import react from 'react'
 import { useSelector } from 'react-redux'
+import { Grid, CircularProgress } from '@material-ui/core';
 
 import Post from './Post/Post'
 import useStyle from './styles'
@@ -11,12 +12,16 @@ const Posts = () => {
   console.log(posts)
   
   return (
-    <>
-        <h1>Posts</h1>
-        <Post />
-        <Post />
-    </>
-  )
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post post={post} setCurrentId={setCurrentId} />
+          </Grid>
+        ))}
+      </Grid>
+    )
+  );
 }
 
 export default Posts
